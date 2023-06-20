b078a6a570cc55c37af0dadab2e6b3af3425b449
tusharb <tusharbansal@iitbhilai.ac.in>
2023-06-20 14:07:13 +0530

    added config to db

---
+++ config/dbConnection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDb = async () => {
+    try{
+      const connect = await mongoose.connect(process.env.MONGODB_CONNNECTION_STRING);
+    }
+    catch(err){
+      console.log(err);
+      process.exit(1);
+    }
+};
+
+module.exports = connectDb;
